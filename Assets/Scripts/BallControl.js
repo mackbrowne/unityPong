773f49f207625ec24096a49d83b924fe04f4b0f1
Mackenzie Browne <mackbrowne@gmail.com>
2014-04-21 00:03:17 -0700

    first commit with content, finished tutorial

---
+++ Assets/Scripts/BallControl.js
@@ -0,0 +1,57 @@
+﻿#pragma strict
+	var bonusSound : AudioSource;
+	var clickSound : AudioSource;
+
+var ballSpeed : float = 100;
+
+function Start () {
+	yield WaitForSeconds(2);
+	GoBall();
+}
+
+function Update(){
+	var xVel : float = rigidbody2D.velocity.x;
+	
+	if(xVel < 18 && xVel > -18 && xVel != 0 ){
+		if(xVel > 0){
+			rigidbody2D.velocity.x = 20;
+		}else{
+			rigidbody2D.velocity.x = -20;
+		}
+	}
+}
+
+function GoBall(){
+	var randomNumber = Random.Range(0, 2);
+	if(randomNumber <= 0.5){
+		rigidbody2D.AddForce(new Vector2(ballSpeed, 10));
+	}else{
+		rigidbody2D.AddForce(new Vector2(-ballSpeed, 10));
+	}
+}
+
+function ResetBall(){
+
+	bonusSound.Play();
+
+	rigidbody2D.velocity.y = 0;
+	rigidbody2D.velocity.x = 0;
+	
+	transform.position.x = 0;
+	transform.position.y = 0;
+	
+	yield WaitForSeconds(0.5);
+	GoBall();
+	
+}
+
+function OnCollisionEnter2D(colInfo : Collision2D){
+	if(colInfo.collider.tag == "Player"){
+		var yVel = rigidbody2D.velocity.y/2 + colInfo.collider.rigidbody2D.velocity.y/3;
+
+		rigidbody2D.velocity.y =  yVel;
+		
+		audio.pitch = Random.Range(0.98f, 1.02f);
+		clickSound.Play();
+	}
+} 
---
+++ Assets/Scripts/GameManager.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+static var playerScore01 : int = 0;
+static var playerScore02 : int = 0;
+
+var theSkin : GUISkin;
+var theBall : Transform;
+
+function Start(){
+	theBall = GameObject.FindGameObjectWithTag("Ball").transform;
+}
+
+static function Score(wallName : String){
+	if(wallName == "RightWall"){
+		playerScore01 += 1;
+	}else{
+		playerScore02 += 1;
+	}
+	Debug.Log("PlayerScore01 -" + playerScore01);
+	Debug.Log("PlayerScore02 -" + playerScore02);
+}
+
+function OnGUI(){
+	GUI.skin = theSkin;
+	GUI.Label(new Rect(Screen.width/2-150-13, 20, 100, 100), "" + playerScore01);
+	GUI.Label(new Rect(Screen.width/2+150-13, 20, 100, 100), "" + playerScore02);
+	
+	if(GUI.Button( new Rect (Screen.width/2-121/2, 29, 121, 53), "RESET" )){
+		playerScore01 = 0;
+		playerScore02 = 0;
+			
+		theBall.gameObject.SendMessage("ResetBall");	
+	}
+}
